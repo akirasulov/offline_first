00d616a2bf7290935dfa5ed348b35f52aa2e40a2
Rasulov Akmal <akmal.rasulov@megafon.tj>
2023-10-01 18:55:48 +0500

    add update functionallity

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ export default {
       editor: null,
       database: null,
       notes: [],
+      activeNote: {},
     };
   },
 
@@ -23,7 +24,7 @@ export default {
 
   mounted() {
     this.editor = new Editor({
-      content: "<p>I’m running Tiptap with Vue.js. 🎉</p>",
+      content: "Your note",
       extensions: [StarterKit],
       editorProps: {
         attributes: {
@@ -58,18 +59,32 @@ export default {
     },
     saveNote() {
       return new Promise((resolve, reject) => {
-        let transaction = this.database.transaction("notes", "readwrite");
-        transaction.oncomplete = (e) => {
-          resolve();
-        };
+        let noteStore = this.database
+          .transaction("notes", "readwrite")
+          .objectStore("notes");
+        let noteRequest = noteStore.get(this.activeNote.created);
 
-        let now = new Date();
-        let note = {
-          content: this.editor.getHTML(),
-          created: now.getTime(),
+        noteRequest.onerror = (e) => {
+          reject("Error saving the note in the database.");
         };
-        this.notes.unshift(note);
-        transaction.objectStore("notes").add(note);
+        noteRequest.onsuccess = (e) => {
+          let note = e.target.result;
+          note.content = this.editor.getHTML();
+
+          let updateRequest = noteStore.put(note);
+
+          updateRequest.onerror = (e) => {
+            reject("Error storing the updated note in the database.");
+          };
+
+          updateRequest.onsuccess = (e) => {
+            let noteIndex = this.notes.findIndex(
+              (n) => n.created == note.created
+            );
+            this.notes[noteIndex] = note;
+            resolve();
+          };
+        };
       });
     },
     getNotes() {
@@ -83,6 +98,31 @@ export default {
         };
       });
     },
+    openNote(note) {
+      this.editor.commands.setContent(note.content);
+      this.activeNote = note;
+    },
+    showAllNotes() {
+      this.editor.commands.clearContent();
+      this.activeNote = {};
+    },
+    addNewNote() {
+      return new Promise((resolve, reject) => {
+        let transaction = this.database.transaction("notes", "readwrite");
+        transaction.oncomplete = (e) => {
+          resolve();
+        };
+
+        let now = new Date();
+        let note = {
+          content: "",
+          created: now.getTime(),
+        };
+        this.notes.unshift(note);
+        this.activeNote = note;
+        transaction.objectStore("notes").add(note);
+      });
+    },
   },
 };
 </script>
@@ -94,12 +134,40 @@ export default {
     >
       <!-- SideBar -->
       <div class="h-0 overflow-auto flex-grow">
+        <div
+          class="flex mt-4 items-center justify-between flex-grow focus:outline-none"
+        >
+          <a
+            class="ml-2 leading-none font-medium text-sm hover:text-gray-900"
+            href=""
+            @click.prevent="showAllNotes"
+            >All Notes</a
+          >
+          <button @click="addNewNote">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-4 h-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+          </button>
+        </div>
+
         <div class="mt-4">
           <a
             class="flex items-center h-8 text-sm pl-8 pr-3"
             href="#"
             v-for="note in notes"
             :key="note.created"
+            @click.prevent="openNote(note)"
           >
             <span class="ml-2 leading-none">
               {{ new Date(note.created).toLocaleString() }}
@@ -108,13 +176,32 @@ export default {
         </div>
       </div>
     </section>
-    <main class="flex flex-col flex-grow">
-      <!-- Main Content -->
+    <main class="flex flex-col flex-grow" v-if="Object.keys(activeNote).length">
+      <!-- Main Content - Editor -->
       <div class="flex flex-col flex-grow overflow-auto">
         <editor-content :editor="editor" />
       </div>
       <div class="h-16 bg-gray-100 border-t border-gray-300 text-right">
         <button @click="saveNote()">Save Note</button>
+      </div>
+    </main>
+    <main class="flex flex-col flex-grow" v-else>
+      <!-- Main Content - Notes List -->
+      <div class="flex flex-col flex-grow overflow-auto">
+        <div v-for="note in notes" :key="note.created">
+          <div class="flex px-4 pt-3 pb-4">
+            <div class="prose my-2 mx-auto">
+              <div>
+                <span class="ml-1 text-xs text-gray-500"
+                  >Created on
+                  {{ new Date(note.created).toLocaleString() }}</span
+                >
+              </div>
+              <div v-html="note.content"></div>
+            </div>
+          </div>
+          <hr class="w-full" />
+        </div>
       </div>
     </main>
   </aside>
